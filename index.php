d9be63881496184c63db01212be0b45b91d766a2
Eikcaj.1998 <jw220998@gmail.com>
2022-08-31 16:19:09 +0200

    add style

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                         </ol>
                         Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.<br>
                         
-                        <h5>Stabilire il paese associato al tuo account</h5>
+                        <h3>Stabilire il paese associato al tuo account</h3>
                         <p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
                             I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
                             Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.</p>
@@ -85,6 +85,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" 
     crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
@@ -100,9 +101,9 @@
     <main>
         <div class="container">
         <?php foreach( $faqs as $faq):?>
-            <h3>
+            <h2>
                 <?php  echo $faq['question']; ?>
-            </h3>
+            </h2>
             <p>
             <?php  echo $faq['answer']; ?>
             </p>
